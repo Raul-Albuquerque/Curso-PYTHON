15e1d1c7481c91979662d502b5ef619c7a98fb02
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-05-25 12:49:03 -0300

    day four

---
+++ day 4/heads_tails.py
@@ -0,0 +1,13 @@
+#HEADS OR TAILS GAME
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+
+#Write the rest of your code below this line 👇
+coin = random.randint(0,1)
+
+if coin == 0:
+    print("Tails")
+else:
+    print("Heads")
---
+++ day 4/randomisation.py
@@ -0,0 +1,12 @@
+import random
+
+#randint -> gera um número inteiro aleatório.
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#random -> gera um float aleatório.
+random_float = random.random()
+print(random_float)
+
+random_float = random_float * 5
+print(random_float)
