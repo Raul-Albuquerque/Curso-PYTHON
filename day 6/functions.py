1dfaa6a891a6dc8bd02ce4af26d98c5e8b9875b8
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-05-29 16:18:31 -0300

    day six

--- day 6/functions.py
+++ day 6/functions.py
@@ -1,4 +1,42 @@
+#defining a function:
 def my_function():
   print("Hello.")
   print("Bye.")
 my_function()
+
+#moving the robot
+#https://reeborg.ca/reeborg.html
+
+#turn_around
+def turn_around
+
+#turn_right
+def turn_right():
+  turn_left()
+  turn_left()
+  turn_left()
+turn_right()
+
+#draw a square
+def square():
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    move()
+square()
+
+#draw a square 2
+def square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+square()
