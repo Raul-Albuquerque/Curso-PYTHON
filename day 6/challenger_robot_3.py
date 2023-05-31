b8302761f08a248b83247741fd2ac483f98a49c1
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-05-30 22:01:26 -0300

    day six

---
+++ day 6/challenger_robot_3.py
@@ -0,0 +1,21 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+#def turn_right():
+   # turn_left()
+   # turn_left()
+   # turn_left()
+    
+#def jump():
+    #if wall_on_right() and front_is_clear():
+       # move()
+    #elif right_is_clear():
+      #  turn_right()
+        #move()
+        #turn_right()
+        #move() 
+  #  else:
+      #  turn_left()
+#while at_goal() != True:
+   # if not wall_in_front() and not right_is_clear():
+      #  move()
+    #else:
+      #  jump()
