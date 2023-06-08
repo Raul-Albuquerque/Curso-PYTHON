8d9e0d37347c0dae5aa7352ce1010617c93f4507
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-06-08 18:00:41 -0300

    day ten

---
+++ day 10/1.function_with_output.py
@@ -0,0 +1,9 @@
+#FUNCTIONS WITH OUTPUTS
+def format_name(f_name, l_name):
+  formated_f_name = (f_name.title())
+  formated_l_name = (l_name.title())
+
+  return f"{formated_f_name} {formated_l_name}"
+
+formated_string = format_name("RAUL", "AlBuQUERQUE")
+print(formated_string)
