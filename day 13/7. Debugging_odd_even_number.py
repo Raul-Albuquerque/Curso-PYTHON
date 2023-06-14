5458d5074d5ca830a2bc2b87a5d21fdf8a1d2299
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-06-14 20:18:53 -0300

    day thirteen

---
+++ day 13/7. Debugging_odd_even_number.py
@@ -0,0 +1,16 @@
+#number = int(input("Which number do you want to check?"))
+
+#if number % 2 = 0:
+  #print("This is an even number.")
+#else:
+#  print("This is an odd number.")
+
+# Há apenas 1 sinal de '=' na comparação, basta incluir mais um sinal de igual.
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
