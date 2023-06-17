7d00db6b5c3e226cd9e68f5eb1bb8ad085a70a29
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-06-17 17:39:32 -0300

    day fourteen

--- day 14/higher_lower_game.py
+++ day 14/higher_lower_game.py
@@ -1,47 +1,42 @@
-from game_data import data
 from art import logo
 from art import vs
+from game_data import data
 import random
+#from replit import clear
 
-
-#functions space
-winner = ''
-def compare(first_person, second_person, users_choice):
+def compare(first_person, second_person, users_choice, score, game_over):
+  winner = ''
   if first_person['follower_count'] > second_person['follower_count']:
     winner = 'A'
-    print(winner)
   else:
     winner = 'B'
-    print(winner)
-  if users_choice == winner:
-    return 'you are right! Current score: '
+  if winner == users_choice:
+    return True
   else:
-    return "Sorry, that's wrong. Final score: "
-    
+    return False
+
+def game ():
+  print(f"Compare A: {first_person['name']}, {first_person['follower_count']}, {first_person['description']}, from {first_person['country']}.")
+  print(vs)
+  print(f"Compare B: {second_person['name']}, {second_person['follower_count']}, {second_person['description']}, from {second_person['country']}.")
+
+score = 0
 game_over = False
-score = ()
+print(logo)
 while not game_over:
   first_person = random.choice(data)
   second_person = random.choice(data)
-  if first_person == second_person:
-    first_person = random.choice(data)
-  
-  #SHOW THE LOGO
-  print(logo)
-  
-  #return the name of first person to dictionary
-  print(f"Compare A: {first_person['name']}, {first_person['description']}, from {first_person['country']}.")
-  
-  #print the 'VS'
-  print(vs)
-  
-  #return the name of second person to dictionary
-  print(f"Compare B: {second_person['name']}, {second_person['description']}, from {second_person['country']}.")
-  
-  #ask who has more followers
-  users_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-  
-  #function with two inputs named compare to return who has more followers
-  print(compare(first_person, second_person, users_choice))
-  print(users_choice)
 
+  if first_person == second_person:
+    second_person = random.choice(data)
+  game()
+  users_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+  score += 1
+  print (compare(first_person, second_person, users_choice, score, game_over))
+  #clear()
+  print(logo)
+  if compare(first_person, second_person, users_choice, score, game_over) == True:
+    print(f"You are right! Current score: {score}.")
+  else:
+    print(f"Sorry, That's wrong. Final score: {score}. ")
+    game_over = True
