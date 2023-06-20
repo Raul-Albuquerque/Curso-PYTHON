dd226b767ea74114cbfc9d33ec2ac0b80fd17cb0
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-06-20 17:01:53 -0300

    day sixteen

---
+++ day 16/2. const_objects.py
@@ -0,0 +1,23 @@
+import turtle
+
+timmy = turtle.Turtle()
+
+#################### SAME THING ###################
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+
+####### ACESSING THE ATRIBUTES #####
+
+# SYNTAX =  object.attribute
+my_screen =  Screen()
+print(my_screen.canvheight) 
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+############ ACESSING THE METHODS #################
+# SYNTAX =  object.method()
+my_screen.exitonclick()
