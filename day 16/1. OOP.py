76f49bdecd54ff8814142ed736004af27a7f719f
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-06-20 16:33:21 -0300

    day sixteen

---
+++ day 16/1. OOP.py
@@ -0,0 +1,18 @@
+# OBJECT ORIENTED PROGRAMMING
+
+#EXAMPLE:
+
+#WAITER IN A RESTAURANT
+
+# HAS -> ATTRIBUTES -> VARIABLE
+is_holding_plate = True
+tables_responsible = [4,5,6]
+
+#DOES -> METHODS -> FUNCTIONS
+#def take_order(table, order):
+    #takes order to chef
+
+#def take_payment(amount):
+    #add money to restaurant
+
+#CLASS AND OBJECT
