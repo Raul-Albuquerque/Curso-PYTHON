fbfb67a591dad39718a4ac695b5fad26f82befd0
raul <raulmalbuquerque2014@gmail.com>
2023-06-24 22:32:31 -0300

    day sixteen

--- day 16/coffee_machine_oop/main.py
+++ day 16/coffee_machine_oop/main.py
@@ -2,14 +2,34 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
 is_on = True
+
 while is_on:
-    drink = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    if drink == "off":
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
         is_on = False
-    elif drink == "report":
-        report = CoffeeMaker().report()
-        print(report)
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
-        enough_resources = CoffeeMaker().is_resource_sufficient(drink)
-        print(enough_resources)
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+                    
+
+
+            
+
+            
+        
+        
+
+        
+    
